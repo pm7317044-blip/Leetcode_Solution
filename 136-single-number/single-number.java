cd1a45cbabe4f1e39c2590ff31eb22d8307e6706
pm7317044-blip <pm7317044@gmail.com>
2026-08-22 10:10:25 +0530

    Time: 537 ms (5.07%) | Memory: 46.9 MB (46.26%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            int count=0;
+            for(int j=0;j<nums.length;j++){
+                if(i!=j&& nums[i]==nums[j]){
+                count++;
+                
+            }
+        }
+        if(count==0){
+            return nums[i];
+        }
+        }
+    return 0;
+}
+}
